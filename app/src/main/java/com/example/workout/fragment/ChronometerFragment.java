0c237a7dbec7f06f5f189242b1073216728b819b
SamuelDidoszak <samueldidoszak1@gmail.com>
2020-12-08 10:40:16 +0100

    V 0.6.6 Breaking down the WorkoutActivity into fragments

---
+++ app/src/main/java/com/example/workout/fragment/ChronometerFragment.java
@@ -0,0 +1,11 @@
+package com.example.workout.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.workout.R;
+
+public class ChronometerFragment extends Fragment {
+    public ChronometerFragment() {
+        super(R.layout.chronometer_fragment);
+    }
+}
---
+++ app/src/main/java/com/example/workout/fragment/CurrentWorkoutFragment.java
@@ -0,0 +1,11 @@
+package com.example.workout.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.workout.R;
+
+public class CurrentWorkoutFragment extends Fragment {
+    public CurrentWorkoutFragment() {
+        super(R.layout.current_workout_fragment);
+    }
+}
